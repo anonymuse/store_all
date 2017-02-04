8a54600d729e479fde48f3f44cdf54455f6cda13
Jesse White <anonymuse@gmail.com>
2017-02-03 20:32:29 -0500

    Add description to script

--- create_key.sh
+++ create_key.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # Create an SSH key for the Freenas root user
+#
+# -N "" tells the script to use an empty passphrase
+# -f %KEYNAME% tells the script where to store the file
+# yes "y" sends an enter style return to the prompts
 
 yes "y" | ssh-keygen -t rsa -b 4096 -N "" -f $HOME/.ssh/freenas_id_rsa
